19cca4450ddf7027d24d47803ad46c866588ed02
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-03-21 21:31:26 +0100

    cleaned up code

--- src/controllers/fees.js
+++ src/controllers/fees.js
@@ -17,13 +17,11 @@ exports.configureFees = catchAsync( async (req, res, next) => {
     let feeConfigs = configParser.parseConfig(FeeConfigurationSpec);
     if (!feeConfigs) return next( new AppError("Could Not Parse Fee Configurations", 400) );
 
-    // await dbUtils.configureFees(feeConfigs);
-    await redisClient.del('Current_Config')
+    // remove current data in disk
+    let currConfig = await redisClient.lRange("Current_Config", 0, 100);
+    if ( currConfig !== null ) await redisClient.del('Current_Config')
     // set data to redis
     await redisClient.lPush("Current_Config",feeConfigs);
-
-    let test = await redisClient.lRange("Current_Config", 0, 100);
-    console.log(test);
 
     res.status(200).send({
         status: "ok"
