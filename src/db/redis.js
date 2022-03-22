f93dbd8cad6fdeee4e5afc80bf103437f1380855
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-03-22 21:47:03 +0100

    DEBUGGING

--- src/db/redis.js
+++ src/db/redis.js
@@ -5,7 +5,13 @@ let REDIS_URI = process.env.REDIS_URI;
 // if ( process.env.NODE_ENV === "production"){
 //     REDIS_URI = process.env.REDIS_URI
 // }
-const redisClient = createClient({url: REDIS_URI});
+const redisClient = createClient({
+    url: REDIS_URI,
+    socket: {
+        tls: true,
+        rejectUnauthorized: false
+    }
+});
 redisClient.connect().catch(console.error)
 
 module.exports = redisClient;
