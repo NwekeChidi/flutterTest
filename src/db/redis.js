ab57391cfc5516b3c8e0f60188119fc38de40f29
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-03-22 21:10:54 +0100

    DEBUGGING

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,10 +1,10 @@
 const { createClient } = require('redis');
 
-let REDIS_URI = process.env.REDIS_LOCAL;
+let REDIS_URI = process.env.REDIS_URI;
 
-if ( process.env.NODE_ENV === "production"){
-    REDIS_URI = process.env.REDIS_URI
-}
+// if ( process.env.NODE_ENV === "production"){
+//     REDIS_URI = process.env.REDIS_URI
+// }
 
 console.log(REDIS_URI);
 console.log(process.env.NODE_ENV)
