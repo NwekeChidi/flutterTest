136aef892fdcfba0f251a2db5d98117e224d0838
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2022-03-22 20:53:26 +0100

    fixed bug in creating client

--- src/db/redis.js
+++ src/db/redis.js
@@ -1,10 +1,11 @@
+const { createClient } = require('redis');
+
 let REDIS_URI = process.env.REDIS_LOCAL;
 
-if ( process.env.NODE_ENV === "production "){
+if ( process.env.NODE_ENV === "production"){
     REDIS_URI = process.env.REDIS_URI
 }
 
-const { createClient } = require('redis')
 const redisClient = createClient(REDIS_URI)
 redisClient.connect().catch(console.error)
 
